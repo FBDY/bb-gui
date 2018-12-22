f2d1841b31ce5182986644e74e3c7c5c0d3a2754
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-12-23 00:46:35 +0300

    Remove leftover code related to Backpack
    Even though the Backpack feature was removed from the GUI, some code
    remained in GUI rendering code. This caused warnings like this to be
    logged to the browser console:
        Warning: React does not recognize the `backpackVisible` prop on a DOM
        element. If you intentionally want it to appear in the DOM as a custom
        attribute, spell it as lowercase `backpackvisible` instead. If you
        accidentally passed it from a parent component, remove it from the DOM
        element.
    This commit removes the offending code and silences such warnings.
    
    There is still some code remaining about Backpack here and there,
    though I don't think it will cause any immediate problems.

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -353,8 +353,6 @@ GUIComponent.propTypes = {
     authorThumbnailUrl: PropTypes.string,
     authorUsername: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]), // can be false
     backdropLibraryVisible: PropTypes.bool,
-    backpackHost: PropTypes.string,
-    backpackVisible: PropTypes.bool,
     basePath: PropTypes.string,
     blocksTabVisible: PropTypes.bool,
     canCreateCopy: PropTypes.bool,
@@ -407,8 +405,6 @@ GUIComponent.propTypes = {
     vm: PropTypes.instanceOf(VM).isRequired
 };
 GUIComponent.defaultProps = {
-    backpackHost: null,
-    backpackVisible: false,
     basePath: './',
     canCreateNew: false,
     canEditTitle: false,
